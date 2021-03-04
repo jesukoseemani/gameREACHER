c944bfbb7cc5b08698aae9841a691c1b82e2d187
jesukoseemani <olowosusiayo@gmail.com>
2021-03-04 04:28:55 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from "./pages/Home"
 import GlobalStyles from "./components/GlobalStyles"
-import {Route} from "react-router-dom"
+import {Route, Redirect} from "react-router-dom"
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
 
@@ -14,6 +14,8 @@ function App() {
      <Route path={["/game/:id", "/"]}>
      <Home />
      </Route>
+
+     <Redirect to= "/" />
 
      <Footer />
     </div>
